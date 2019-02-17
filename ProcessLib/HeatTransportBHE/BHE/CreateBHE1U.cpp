9240ec3e7ec2dc1910a6453c7e0a5894866447c9
Haibing Shao <haibing.shao@ufz.de>
2019-02-17 17:47:00 +0100

    add the parameter ifUsePythongBC to base class,update create function

--- ProcessLib/HeatTransportBHE/BHE/CreateBHE1U.cpp
+++ ProcessLib/HeatTransportBHE/BHE/CreateBHE1U.cpp
@@ -24,6 +24,17 @@ BHE::BHE_1U createBHE1U(
              std::unique_ptr<MathLib::PiecewiseLinearInterpolation>> const&
         curves)
 {
+    // if the BHE is using python boundary condition
+    bool bhe_if_use_python_bc = false;
+    if (auto const bhe_if_use_python_bc_conf =
+            config.getConfigParameterOptional<bool>(
+                "bhe_if_use_python_bc"))
+    {
+        DBUG("If  using python boundary condition : %s",
+             (*bhe_if_use_python_bc_conf) ? "true" : "false");
+        bhe_if_use_python_bc = *bhe_if_use_python_bc_conf;
+    }
+
     //! \ogs_file_param{prj__processes__process__HEAT_TRANSPORT_BHE__borehole_heat_exchangers__borehole_heat_exchanger__borehole}
     auto const borehole_geometry =
         createBoreholeGeometry(config.getConfigSubtree("borehole"));
@@ -58,8 +69,7 @@ BHE::BHE_1U createBHE1U(
         curves,
         refrigerant);
 
-    return {borehole_geometry, refrigerant, grout, flowAndTemperatureControl,
-            pipes};
+    return {borehole_geometry, refrigerant, grout, flowAndTemperatureControl, pipes, bhe_if_use_python_bc};
 }
 }  // namespace BHE
 }  // namespace HeatTransportBHE
